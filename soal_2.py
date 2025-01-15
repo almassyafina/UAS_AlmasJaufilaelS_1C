5f5076477dde6545a39437d4233168ca9d7bb4e5
almassyafina <almasjaufilael.25@gmail.com>
2025-01-15 12:30:05 +0700

    Add files via upload

--- soal_2.py
+++ soal_2.py
@@ -1,24 +1,19 @@
 import pandas as pd
 
-data ={
-    'Nama':['Mahasiswa1','Mahasiswa2','Mahasiswa3'],
-    'Algoritma dan struktur data 2' :[90,50,65],
-    'Matematika Numerik': [80,60,70]
-}
-df= pd.DataFrame[
-    'Nama':['Mahasiswa1','Mahasiswa2','Mahasiswa3'],
-    'Algoritma dan struktur data 2' :[90,50,65],
-    'Matematika Numerik': [80,60,70]
-]
+
+df= pd.DataFrame(
+    [90,50,65],
+    [80,60,70]
+)
 index=['Mahasiswa1','Mahasiswa2','Mahasiswa3'],
 columns=['Nama','Algoritma dan struktur data 2','Matematika Numerik']
 
 df['Algoritma'] = [90,50,65]
 df['Matematika Numerik'] = [80,60,70]
 
-df['total'] = df.sum(axis=1)
 
 df.loc['total'] = df.sum(axis=0)
+df['rata']=df.mean(axis=1)
 
 print(df)
 
